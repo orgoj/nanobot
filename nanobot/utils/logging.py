4c7585548750068342a7f74e492fd0f4e88a24dd
Michael Heca <michael@heca.cz>
2026-02-15 19:23:10 +0100

    Update logging.py

--- nanobot/utils/logging.py
+++ nanobot/utils/logging.py
@@ -65,7 +65,11 @@ def setup_logging(config: Config):
     logger.add(
         str(log_file),
         level=config.logging.level,
-        format=lambda r: jsonl_serializer(r).replace("{", "{{").replace("}", "}}"),
+        format=lambda r: jsonl_serializer(r)
+        .replace("{", "{{")
+        .replace("}", "}}")
+        .replace("<", "\\<")
+        .replace(">", "\\>"),
         colorize=False,
     )
 
